ea6828320a9eaecaafd0b74f12e5d89ca31dd0d0
Audaxxxy <nathan.boldy@ontariotechu.net>
2020-11-20 23:01:41 -0500

    Polish, optimizations, explosions

--- Assets/Scripts/CrashCar.cs
+++ Assets/Scripts/CrashCar.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CrashCar : MonoBehaviour
 {
+    public GameObject crashParticle;
     //This script is attached to 'out of bounds' colliders - usually meaning the edge of the track.
     //Touching this will automatically 'crash' the car, and halt its score.
     private void OnCollisionEnter(Collision collision)
@@ -13,6 +15,7 @@ public class CrashCar : MonoBehaviour
         if (collision.collider.TryGetComponent(out cc))
         {
             cc.Crashed = true;
+            cc.GetComponentInChildren<ParticleSystem>(true).gameObject.SetActive(true);
         }
     }
 }
